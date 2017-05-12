b24702f5ee56d35599f957054cc7dec1dcf0d0bd
David Ashirov <david.ashirov@generalassemb.ly>
2017-05-12 08:33:25 -0400

    Add snowplow event logging classes ( StepStatus requires Account - may need to be revised )

---
+++ src/main/java/batch/JobFailed.java
@@ -0,0 +1,15 @@
+package batch;
+
+/**
+ * Created by dashirov on 1/12/17.
+ */
+public class JobFailed extends JobState{
+   public JobFailed() {
+      super();
+      setIglu(State.FAILED.name());
+   }
+   public JobFailed(String runId) {
+      super(runId);
+      setIglu(State.FAILED.name());
+   }
+}
