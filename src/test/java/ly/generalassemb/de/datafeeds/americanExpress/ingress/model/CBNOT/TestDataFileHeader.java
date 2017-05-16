baf6806922fa1c5b9856fd3acdcca99e00c481b0
David Ashirov <david.ashirov@generalassemb.ly>
2017-05-16 14:45:58 -0400

    Added CBNOT (Charge Back Notification) data file format    - Header record (complete)    - Detail record (TBD)    - Trailer record (complete)

--- src/test/java/ly/generalassemb/de/datafeeds/americanExpress/ingress/model/CBNOT/TestDataFileHeader.java
+++ src/test/java/ly/generalassemb/de/datafeeds/americanExpress/ingress/model/CBNOT/TestDataFileHeader.java
@@ -15,7 +15,7 @@ import java.util.TimeZone;
 public class TestDataFileHeader {
     @Test
     public void testParser() {
-        String input = "HXXFF20170515                                                                                        123456CBNOT20172380224012                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ";
+        String input = "H010120170515                                                                                        123456CBNOT20172380224012                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ";
         DataFileHeader record = null;
         try {
             record = DataFileHeader.parse(input);
@@ -25,14 +25,14 @@ public class TestDataFileHeader {
         Assert.assertNotNull(record);
         Assert.assertEquals("H", record.getDataFileHeaderRecordType());
         Assert.assertEquals(2202, input.length());
-        Assert.assertEquals("XX", record.getAmexApplication().getApplicationSytemCode());
-        Assert.assertEquals("FF", record.getAmexApplication().getFileTypeCode());
+        Assert.assertEquals("01", record.getAmexApplication().getApplicationSytemCode());
+        Assert.assertEquals("01", record.getAmexApplication().getFileTypeCode());
         try {
             Assert.assertEquals(new SimpleDateFormat("yyyy-MM-dd").parse("2017-05-15"), record.getAmexApplication().getFileCreationDate());
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        Assert.assertEquals("{\"applicationSytemCode\":\"XX\",\"fileTypeCode\":\"FF\",\"fileCreationDate\":\"2017-05-15\"}", record.getAmexApplication().toString());
+        Assert.assertEquals("{\"applicationSytemCode\":\"01\",\"fileTypeCode\":\"01\",\"fileCreationDate\":\"2017-05-15\"}", record.getAmexApplication().toString());
         Assert.assertEquals("CBNOT", record.getDataType());
         try {
             Assert.assertEquals(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2017-08-26 22:40:12"), record.getOriginalFileTransmissionDateTime());
@@ -41,7 +41,7 @@ public class TestDataFileHeader {
         }
         Assert.assertEquals("123456", record.getServiceAccessId());
 
-        Assert.assertEquals("{\"dataFileHeaderRecordType\":\"H\",\"amexApplication\":{\"applicationSytemCode\":\"XX\",\"fileTypeCode\":\"FF\",\"fileCreationDate\":\"2017-05-15\"},\"serviceAccessId\":\"123456\",\"dataType\":\"CBNOT\",\"originalFileTransmissionDateTime\":\"2017-08-26 22:40:12.000-0400\"}", record.toString());
+        Assert.assertEquals("{\"dataFileHeaderRecordType\":\"H\",\"amexApplication\":{\"applicationSytemCode\":\"01\",\"fileTypeCode\":\"01\",\"fileCreationDate\":\"2017-05-15\"},\"serviceAccessId\":\"123456\",\"dataType\":\"CBNOT\",\"originalFileTransmissionDateTime\":\"2017-08-26 22:40:12.000-0400\"}", record.toString());
 
     }
 }
